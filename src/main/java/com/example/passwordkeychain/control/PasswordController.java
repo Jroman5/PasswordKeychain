5a770e04ed4496fb536f4e4747d37f420700c5ed
Jroman5 <jonathan.roman519@gmail.com>
2021-10-25 14:53:21 -0600

    Updated PasswordController such that it only retrieves and displays information. Moved Account creation to AccountHandler class moved account updating to AccountHandler class. Updated onItemSelect, onSaveButtonClick, onDeleteButtonClick to check for empty Lists.

--- src/main/java/com/example/passwordkeychain/control/PasswordController.java
+++ src/main/java/com/example/passwordkeychain/control/PasswordController.java
@@ -63,6 +63,7 @@ public class PasswordController {
         if(accountHandler.isEmpty()){
             return;
         }
+
         accountHandler.removeAccount(websites.getSelectionModel().getSelectedIndex());
 
     }
